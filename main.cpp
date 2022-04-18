0951d0fb7cc47bff33e44bec2260ef85b909a6a7
alejandrodelcs <alejodc15@gmail.com>
2022-04-17 23:09:48 -0300

    cambios

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include "utiles.h"
 
 
-int main(int argc, char* argv[]) {
-    buscarPalabra((argv[2]),argv[1]);
+int main(int argc, char *argv[]) {
+    if (argc < 3)
+        std::cout << "No ingreso argumentos." << std::endl;
+    else
+        buscarPalabra((argv[2]), argv[1]);
+
     return 0;
 }
