ce509ae291b11d4f5038ec263bffcd6ccaf32b4b
alejandrodelcs <alejodc15@gmail.com>
2022-04-18 08:19:19 -0300

    cambios

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char *argv[]) {
     if (argc < 3)
-        std::cout << "No ingreso argumentos." << std::endl;
+        std::cout << "Argumentos incorrectos" << std::endl;
     else
         buscarPalabra((argv[2]), argv[1]);
 
